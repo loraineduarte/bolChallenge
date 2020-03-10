3575c248231ab30b491aa522df1ef57942a33eee
Loraine <loraine@ciandt.com>
2020-03-10 17:43:03 -0300

    all informations on html

--- src/main/java/bol/project/java/controller/MancalaController.java
+++ src/main/java/bol/project/java/controller/MancalaController.java
@@ -3,7 +3,9 @@ package bol.project.java.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import bol.project.java.model.Mancala;
 
@@ -11,19 +13,24 @@ import bol.project.java.model.Mancala;
 public class MancalaController {
 
   @GetMapping("/")
-  public String mancala(Model model) {
-    return "mancala";
-  }
-
-  @PostMapping("/")
   public String index(Model model) {
-
     Mancala mancalaBoard = new Mancala();
 
-    model.addAttribute("p1_mancala", mancalaBoard.p1_mancala);
-    model.addAttribute("p1_pitsa", mancalaBoard.p1_pits);
-    model.addAttribute("p2_mancala", mancalaBoard.p2_mancala);
-    model.addAttribute("p2_pits", mancalaBoard.p2_pits);
+    model.addAttribute("mancala_P1", mancalaBoard.getMancalaBoard()[0]);
+    model.addAttribute("P1_pit1", mancalaBoard.getMancalaBoard()[1]);
+    model.addAttribute("P1_pit2", mancalaBoard.getMancalaBoard()[2]);
+    model.addAttribute("P1_pit3", mancalaBoard.getMancalaBoard()[3]);
+    model.addAttribute("P1_pit4", mancalaBoard.getMancalaBoard()[4]);
+    model.addAttribute("P1_pit5", mancalaBoard.getMancalaBoard()[5]);
+    model.addAttribute("P1_pit6", mancalaBoard.getMancalaBoard()[6]);
+
+    model.addAttribute("mancala_P2", mancalaBoard.getMancalaBoard()[7]);
+    model.addAttribute("P2_pit1", mancalaBoard.getMancalaBoard()[8]);
+    model.addAttribute("P2_pit2", mancalaBoard.getMancalaBoard()[9]);
+    model.addAttribute("P2_pit3", mancalaBoard.getMancalaBoard()[10]);
+    model.addAttribute("P2_pit4", mancalaBoard.getMancalaBoard()[11]);
+    model.addAttribute("P2_pit5", mancalaBoard.getMancalaBoard()[12]);
+    model.addAttribute("P2_pit6", mancalaBoard.getMancalaBoard()[13]);
 
     return "mancala";
   }
--- src/main/java/bol/project/java/model/Mancala.java
+++ src/main/java/bol/project/java/model/Mancala.java
@@ -1,62 +1,30 @@
 package bol.project.java.model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Mancala {
 
-  public int p1_mancala = 0;
-  public Map<Integer, Integer> p1_pits = new HashMap<Integer, Integer>();
-
-  public int p2_mancala = 0;
-  public Map<Integer, Integer> p2_pits = new HashMap<Integer, Integer>();
+  int[] mancalaBoard = {0,6,6,6,6,6,6,0,6,6,6,6,6,6};
 
   public Mancala() {
-    p1_pits.put(1, 6);
-    p1_pits.put(2, 6);
-    p1_pits.put(3, 6);
-    p1_pits.put(4, 6);
-    p1_pits.put(5, 6);
-    p1_pits.put(6, 6);
-
-    p2_pits.put(1, 6);
-    p2_pits.put(2, 6);
-    p2_pits.put(3, 6);
-    p2_pits.put(4, 6);
-    p2_pits.put(5, 6);
-    p2_pits.put(6, 6);
   }
 
-  public int getP1_mancala() {
-    return p1_mancala;
+  public int[] getMancalaBoard() {
+    return mancalaBoard;
   }
 
-  public void setP1_mancala(int p1_mancala) {
-    this.p1_mancala = p1_mancala;
+  public void setMancalaBoard(int[] mancalaBoard) {
+    this.mancalaBoard = mancalaBoard;
   }
 
-  public Map<Integer, Integer> getP1_pits() {
-    return p1_pits;
-  }
+  // public void p1Plays(int position) {
 
-  public void setP1_pits(Map<Integer, Integer> p1_pits) {
-    this.p1_pits = p1_pits;
-  }
+  // }
 
-  public int getP2_mancala() {
-    return p2_mancala;
-  }
+  // public void p2Plays(int position) {
 
-  public void setP2_mancala(int p2_mancala) {
-    this.p2_mancala = p2_mancala;
-  }
-
-  public Map<Integer, Integer> getP2_pits() {
-    return p2_pits;
-  }
-
-  public void setP2_pits(Map<Integer, Integer> p2_pits) {
-    this.p2_pits = p2_pits;
-  }
-
+  // }
 }
